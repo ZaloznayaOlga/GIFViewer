d78c4270256b6d2d4d763617f87358fc2babc1b9
Olga Zaloznaya <zaloznaya.o@udtech.global>
2021-07-29 03:44:47 +0300

    Create ImageFragment & add navigation

--- app/src/main/java/zaloznaya/olga/app/gifviewer/domain/model/GifImage.kt
+++ app/src/main/java/zaloznaya/olga/app/gifviewer/domain/model/GifImage.kt
@@ -1,8 +1,12 @@
 package zaloznaya.olga.app.gifviewer.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GifImage(
     val id: String,
     val title: String,
     val url_preview: String,
     val url_original: String
-)
+) : Parcelable
